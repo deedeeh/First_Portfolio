57e08321b30feb49d91e65ac50a2fa58c768f801
Dina Hafez <dinahafez.8@gmail.com>
2017-10-14 19:09:00 +0100

    Add javascript for header when scroll
    I have managed to work on the scrolling part for the header, to add
    background colour but still I need to fix few things.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@
 var header = document.querySelector("header");
 var container = document.querySelector(".container");
 
-var distance = container.offsetHeight - header.offsetHeight;
 
-if(window.pageYOffset >= distance) {
-  header.classList.add("has-background");
-}
 
-console.log(distance);
+window.addEventListener('scroll', function() {
+  var distance = container.offsetHeight - header.offsetHeight;
+  if(window.pageYOffset >= distance) {
+    header.classList.add("has-color");
+  } else {
+    header.classList.remove("has-color");
+  }
+});
