76dfa08dd4fc5aa3004a4161e388b240e747a548
Dina Hafez <dinahafez.8@gmail.com>
2017-10-25 23:11:05 +0100

    Add link about nav to section version one
    This was during my code workshop with Jo. I was working on linking the
    navigation link about with the section and have the correct height. I
    am still working on it and also on the way the work section background
    color isn’t working the way it supposed to be. Still need to work on it.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 
-var header = document.querySelector("header");
+var body = document.querySelector("body");
 var container = document.querySelector(".container");
+var header = document.querySelector("header");
+var aboutLink = document.getElementById("aboutLink");
+var workLink = document.getElementById("workLink");
 
 window.addEventListener('scroll', function() {
   var distance = container.offsetHeight - header.offsetHeight;
   if(window.pageYOffset >= distance) {
-    header.classList.add("has-color");
+    body.classList.add("has-color");
   } else {
-    header.classList.remove("has-color");
+    body.classList.remove("has-color");
   }
+});
+
+window.addEventListener("load", function() {
+  container.style.height = window.innerHeight + "px";
+  var top = document.getElementById("about").getBoundingClientRect().top - 60;
+  aboutLink.addEventListener("click", function(e) {
+    e.preventDefault();
+    window.scrollTo(0, top);
+    console.log(top);
+  });
 });
