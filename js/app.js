4f24f400206a928befe0c8d58cec159132def254
Dina Hafez <dinahafez.8@gmail.com>
2017-10-29 20:24:09 +0000

    Fix about and work problem with padding
    The problem I was experiencing with small screens when I click on the
    about or work link in the navigation some of the sections content were
    hidden under the header background color. I tried to do it in the
    workshop last time but the solution we came to wasn’t working all the
    time. I thought about why not add some padding before the content of
    each section so when I click this amount of whitespace will be hidden
    under the header and the beginning of the content as text or image will
    appear just after the header. It worked and it served its purpose but I
    believe there might be a way to do it with js. I will try to find out
    next workshop what could be done.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var header = document.querySelector("header");
 var aboutLink = document.getElementById("aboutLink");
 var workLink = document.getElementById("workLink");
 
+
 window.addEventListener('scroll', function() {
   var distance = container.offsetHeight - header.offsetHeight;
   if(window.pageYOffset >= distance) {
@@ -14,12 +15,13 @@ window.addEventListener('scroll', function() {
   }
 });
 
-window.addEventListener("load", function() {
-  container.style.height = window.innerHeight + "px";
-  var top = document.getElementById("about").getBoundingClientRect().top - 90;
-  aboutLink.addEventListener("click", function(e) {
-    e.preventDefault();
-    window.scrollTo(0, top);
-    console.log(top);
-  });
-});
+
+// window.addEventListener("load", function() {
+//   var top = document.getElementById("about").getBoundingClientRect().top - 90;
+//   container.style.height = window.innerHeight + "px";
+//   aboutLink.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     window.scrollTo(0, top);
+//     console.log(top);
+//   });
+// });
