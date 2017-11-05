81e820adf95bbfd28d2c4b2c856c2097412e89a3
Dina Hafez <dinahafez.8@gmail.com>
2017-11-05 21:33:00 +0000

    Remove some codes in javascript
    I commented out some scripts that I don’t need anymore so I deleted
    them.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@
 var body = document.querySelector("body");
 var container = document.querySelector(".container");
 var header = document.querySelector("header");
-var aboutLink = document.getElementById("aboutLink");
-var workLink = document.getElementById("workLink");
-
 
 window.addEventListener('scroll', function() {
   var distance = container.offsetHeight - header.offsetHeight;
@@ -14,14 +11,3 @@ window.addEventListener('scroll', function() {
     body.classList.remove("has-color");
   }
 });
-
-
-// window.addEventListener("load", function() {
-//   var top = document.getElementById("about").getBoundingClientRect().top - 90;
-//   container.style.height = window.innerHeight + "px";
-//   aboutLink.addEventListener("click", function(e) {
-//     e.preventDefault();
-//     window.scrollTo(0, top);
-//     console.log(top);
-//   });
-// });
